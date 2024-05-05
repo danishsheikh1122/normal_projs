4e7b57c907fc740d88b9910c09b89a431e29be60
danishsheikh1122 <mohammeddanishsheikh1122@gmail.com>
2024-05-05 12:00:00 +0000

    heart

--- aa.js
+++ aa.js
@@ -32,7 +32,7 @@ function generateCommitsForPreviousMonth() {
   
     const year = today.getFullYear();
     const month = today.getMonth(); // Note: January is 0, December is 11
-  
+   
       
     // Calculate previous month's year and month   
         
