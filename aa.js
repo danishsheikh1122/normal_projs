571523af92727460ad2cd6dfaf38a3e6394b873b
danishsheikh1122 <mohammeddanishsheikh1122@gmail.com>
2024-11-25 14:30:12 +0000

    Demo commit

--- aa.js
+++ aa.js
@@ -57,7 +57,7 @@ function gitAutoCommit(date, message) {
 
 // Get today's date in the ISO format
 const today = new Date();
-const hardcodedDate = '2024-11-25T14:30:11Z'; // Fixed time of day
+const hardcodedDate = '2024-11-25T14:30:12Z'; // Fixed time of day
 console.log(hardcodedDate)
 const commitMessage = 'Demo commit';
 
